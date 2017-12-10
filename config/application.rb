d43d0d13cbbe460a63ecb84bc4b1c974036b4d40
Kakiuchi Shusuke <kakiuchis@gmail.com>
2017-12-10 18:01:54 +0900

    time change

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Whenevertest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.time_zone = 'Tokyo'
+    config.time_zone = 'Tokyo'
   end
 end
